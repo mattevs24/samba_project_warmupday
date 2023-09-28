96ac6445f3f33f52084fec466549cd99944d5660
Matthew Evans <matt.evans2411@gmail.com>
2023-09-28 14:21:13 +0100

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def rand_vec(n: int) -> np.ndarray:
+    """
+    Function to generate a vector of length $n$ with uniformly distributed variabels between 0 and 1
+    :param n:
+    :return:
+    """
+    return np.random.uniform(size=n)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    vec = rand_vec(10)
+    print(vec)
