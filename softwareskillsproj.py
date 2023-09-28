6e8990c02a0129123f2a6acc3b4049ece7978f17
dokane19 <daireokane@gmail.com>
2023-09-28 16:04:46 +0100

    test

--- softwareskillsproj.py
+++ softwareskillsproj.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import sympy as sp
-#x=np.linspace(0,1,100)
+#x=np.linspace(0,1,100
 #y=np.linspace(0,1,100)
 #axes = plt.axes()
 #ax = plt.axes(projection="3d")
