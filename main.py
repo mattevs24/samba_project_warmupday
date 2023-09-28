4f09eddbc93ce0540f8fad7affd9e190d429080a
Matthew Evans <matt.evans2411@gmail.com>
2023-09-28 17:02:25 +0100

    final image too

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import scipy.stats as stats
 
 
 def naive_points_generation(density: stats.rv_continuous,
-                            lower_bound: float, upper_bound: float, n: int, **kwargs) -> np.ndarray:
+                            lower_bound: float, upper_bound: float, n: int, kwargs: dict) -> np.ndarray:
     points = density.rvs(size=n, **kwargs)
     lower_check = points >= lower_bound
     upper_check = points <= upper_bound
@@ -28,15 +28,15 @@ def func1(x, y):
 
 
 def first_integration(integrand: typing.Callable[[np.ndarray, np.ndarray], np.ndarray],
-                      density: stats.rv_continuous, lower_bound: float = 0.0,
+                      density: stats.rv_continuous, kwargs: dict, lower_bound: float = 0.0,
                       upper_bound: float = 1.0, n: int = 100000) -> float:
 
-    vec_x = naive_points_generation(density, lower_bound, upper_bound, n)
-    vec_y = naive_points_generation(density, lower_bound, upper_bound, n)
+    vec_x = naive_points_generation(density, lower_bound, upper_bound, n, kwargs)
+    vec_y = naive_points_generation(density, lower_bound, upper_bound, n, kwargs)
 
     _eval_top = integrand(vec_x, vec_y)
-    _eval_bottom = density.pdf(vec_x) * density.pdf(vec_y)
-    correction_term = (density.cdf(upper_bound) - density.cdf(lower_bound))**2
+    _eval_bottom = density.pdf(vec_x, **kwargs) * density.pdf(vec_y, **kwargs)
+    correction_term = (density.cdf(upper_bound, **kwargs) - density.cdf(lower_bound, **kwargs))**2
 
     _eval = _eval_top / _eval_bottom * correction_term
     _eval = _eval.mean()
@@ -45,5 +45,5 @@ def first_integration(integrand: typing.Callable[[np.ndarray, np.ndarray], np.nd
 
 
 if __name__ == "__main__":
-    out = first_integration(func1, stats.norm)
+    out = first_integration(func1, stats.norm, kwargs={})
     print(out)
--- plotting_functions.py
+++ plotting_functions.py
@@ -36,31 +36,33 @@ def plot_more_efficient(integrand: typing.Callable[[np.ndarray, np.ndarray], np.
 
 
 def plot_unif_norm_convergences(integrand: typing.Callable[[np.ndarray, np.ndarray], np.ndarray],
-                                max_n: int, samples: int):
+                                max_n: int, samples: int, **kwargs):
     outputs_unif = []
     outputs_norm = []
     running_total_samples = samples
     running_sum_unif = 0.0
     running_sum_norm = 0.0
     for _ in np.arange(100, max_n, samples):
-        out_unif = main.first_integration(integrand, stats.uniform, 0.0, 1.0, samples)
-        out_norm = main.first_integration(integrand, stats.norm, 0.0, 1.0, samples)
+        out_unif = main.first_integration(integrand, stats.uniform, {}, 0.0, 1.0, samples)
+        out_norm = main.first_integration(integrand, stats.norm, kwargs, 0.0, 1.0, samples)
         out_unif *= float(samples)
         out_norm *= float(samples)
         running_sum_unif += out_unif
         running_sum_norm += out_norm
-        running_total_samples += samples
 
         out_unif = running_sum_unif / float(running_total_samples)
         out_norm = running_sum_norm / float(running_total_samples)
         outputs_unif.append([out_unif])
         outputs_norm.append([out_norm])
 
+        running_total_samples += samples
+
     labels = np.arange(100, max_n, samples)
     plt.plot(labels, outputs_unif, 'r')
     plt.plot(labels, outputs_norm, 'g')
+    plt.savefig("output1.png")
     plt.show()
 
 
 if __name__ == "__main__":
-    plot_unif_norm_convergences(main.func1, 5000, samples=100)
+    plot_unif_norm_convergences(main.func1, 50000, samples=100)
